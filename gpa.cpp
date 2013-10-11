233fb68c64dae89053aa15ae02399361e27a562c
Henry <huangh>
2013-10-11 00:04:50 -0400

    update flesh_out with latest logic

--- gpa.cpp
+++ gpa.cpp
@@ -12,55 +12,54 @@ Features to add:
 -modularize
 -crawl BU website for graduation GPA requirements (woahhh)
 
-flesh_out branch - flesh out logic and error checking
+update flesh_out branch - merged incorrectly before
 
 ******************************************************************/
 
 #include <iostream>
 #include "math.h"
 
-using namespace std; 
 
 int main(){
 
-double gpa, gpa_want, gpa_need, credit_remain, honor; 
-double grad_credit = 132; 
+double gpa = 0.0, //current GPA on 4.0 scale
+	gpa_want = 0.0, //desired GPA (4.0 scale)
+	gpa_need = 0.0; //calculated GPA value needed for rest of school career
+//	credit_remain = 0.0, //calculated number of credits 
 
-cout << "Enter your GPA: "; 
-cin >> gpa; 
-while(gpa < 0 || gpa > 4)
+int grad_credit = 132, //credits required to graduate for engineering
+	credits = 0;
+
+std::cout << "Enter your current GPA (4.0 scale): "; 
+std::cin >> gpa; 
+while(gpa < 0.0 || gpa > 4.0)
 {
-  cout << "Invalid GPA. Enter your GPA on the 4.0 scale: ";
-  cin >> gpa;
+  std::cout << "Invalid GPA. Enter your GPA on the 4.0 scale: ";
+  std::cin >> gpa;
 } 
 
-cout << "Enter the number of \"Honor Points\" you have: "; 
-cin >> honor;
-while(honor < 0 || honor > 4*grad_credit)
+std::cout << "Enter your total number of credit hours taken thus far (about ~32 per year): "; 
+std::cin >> credits;
+while(credits < 0 || credits > 132)
 {
-  cout << "You can't have that many honor points. Enter a valid number of HP under 528: ";
-  cin >> honor;
+  std::cout << "You can't have that many. Again please: ";
+  std::cin >> credits;
 } 
 
-cout << "What is your desired GPA upon graduation? ";
-cin >> gpa_want;
+std::cout << "What GPA do you want to graduate with? ";
+std::cin >> gpa_want;
 while(gpa_want < 0 || gpa_want > 4)
 {
-  cout << "You can't get that GPA...makes no sense. Enter a valid GPA between 0.0 and 4.0: ";
-  cin >> gpa_want;
+  std::cout << "You can't get that GPA...makes no sense. Enter a valid GPA between 0.0 and 4.0: ";
+  std::cin >> gpa_want;
 } 
 
-gpa_need = ((gpa_want*grad_credit) - honor)/(grad_credit - honor/gpa);
+gpa_need = (gpa_want*grad_credit - gpa*credits)/(grad_credit - credits);
 
-cout << "The GPA you need to get to achieve a " << gpa_wants << "overall GPA is a \n" << gpa_need << "GPA for the remaining " << honor/gpa << "credits till you graduate" << endl;
+std::cout << "The GPA you need to get to achieve a " << gpa_want << " overall GPA is a \n" << gpa_need << " GPA for the remaining " << grad_credit - credits << "credits till you graduate" << std::endl;
 
+if( gpa_need > 4.0)
+	std::cout << "Sorry bro/bra, can't be done" << std::endl;
 
 system("pause");
-
-
-
-
-
-
-
 }
