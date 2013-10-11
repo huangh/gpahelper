5386bdcf2c220c813d0e448467ae33e3c47aa8f6
Henry <huangh>
2013-10-10 23:31:13 -0400

    added comments to the top

--- gpa.cpp
+++ gpa.cpp
@@ -1,3 +1,21 @@
+/******************************************************************
+By: huangh
+initially made this to learn git...might as well make it better. 
+
+calculates for you the GPA you need to get a certain goal GPA by
+the time you graduate. 
+
+Features to add: 
+-figure out combination of class grades that could add up to desired GPA
+-good error checking
+-gui
+-modularize
+-crawl BU website for graduation GPA requirements (woahhh)
+
+gpa gui branch
+
+******************************************************************/
+
 #include <iostream>
 #include "math.h"
 
